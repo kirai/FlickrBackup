1d3663bef896169bfc02e17d663cee63f5116d11
Hector Garcia <hectorgarcia@gmail.com>
2013-03-26 13:02:58 +0900

    Refactoring like a boss #infernal_one_liner

--- lib/exif_helper.rb
+++ lib/exif_helper.rb
@@ -2,7 +2,6 @@ require 'mini_exiftool'
 
 class ExifHelper
 
-
   # Use this to transform an array of tags from a FlickRaw::ResponseList to a string
   # of comma separated tags
   #
@@ -10,9 +9,7 @@ class ExifHelper
   #
 
   def self.flickr_tags_to_string(tags)
-    tags_string = ""
-    tags.each { |tag| tag != tags.last ? tags_string << (tag["raw"] + ', ') : tags_string << tag["raw"] } unless tags.empty?
-    return tags_string
+    return tags.inject("") { |tags_string, tag| tag != tags.last ? tags_string << (tag["raw"] + ', ') : tags_string << tag["raw"] }
   end
 
   # Use this to edit the exif information of an image
